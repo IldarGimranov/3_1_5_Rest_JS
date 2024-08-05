5e4aa7aa498ff4b2865521482587a05c4b054334
IldarGimranov <164064433+IldarGimranov@users.noreply.github.com>
2024-08-05 23:59:40 +0500

    rest js

--- src/main/java/ru/kata/spring/boot_security/demo/configurations/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configurations/MvcConfig.java
@@ -1,8 +1,10 @@
 package ru.kata.spring.boot_security.demo.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -10,5 +12,12 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/user").setViewName("user");
         registry.addViewController("/welcome").setViewName("index");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/js/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());;
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,29 +1,36 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 import java.security.Principal;
 
 
-@Controller
-@RequestMapping()
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
 
-    private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public User readUser(@AuthenticationPrincipal User user) {
+        return user;
     }
 
-    @GetMapping("/user")
-    public String readUser(Model model, Principal principal) {
-        model.addAttribute("user", userService.findByUsername(principal.getName()));
-        return "user";
-    }
+//    private final UserService userService;
+//
+//    @Autowired
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/user")
+//    public String readUser(Model model, Principal principal) {
+//        model.addAttribute("user", userService.findByUsername(principal.getName()));
+//        return "user";
+//    }
 }
